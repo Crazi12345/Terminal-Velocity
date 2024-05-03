f4140de6e3a2fe45c858153e234886c7bacea437
Patrick <patrickbjerre@outlook.dk>
2024-05-03 12:28:14 +0200

    binary now workds

--- cheaterSheater/chtsht/src/main.rs
+++ cheaterSheater/chtsht/src/main.rs
@@ -2,11 +2,22 @@ use std::fs::File;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 fn main() {
-
     let args: Vec<String> = std::env::args().collect();
     let lang: &str = args.get(1).expect("only had one");
-//    let option: &str = args.get(2).expect("");
-    let result = Command::new("curl").args(["cht.sh/",stringify!("{}",lang)]).exec();
-    println!("{}",result);
+    let option = args.get(2);
+    if !option.is_none() {
+        let search = option.unwrap();
+        let result = Command::new("curl")
+            .args(["cht.sh/".to_owned() + lang + "/" + search])
+            .exec();
+        let run = Command::new(result.to_string()+"> command.txt").exec();
+        println!("{}", result);
+        println!("{}", run);
+    } else {
+        let result = Command::new("curl")
+            .args(["cht.sh/".to_owned() + lang])
+            .exec();
 
+        println!("{}", result);
+    }
 }
